08e710af4c037233b6879d0bdc7ad8899f832020
unknown <drissaitkassi@gmail.com>
2022-12-23 15:28:27 +0000

    implementer trouver location par id

---
+++ src/mapCourse/Location.java
@@ -0,0 +1,46 @@
+package mapCourse;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Location {
+    private int locationId;
+    private String description;
+    private Map<String ,Integer> exits;
+
+    public Location(int locationId, String description,Map<String ,Integer> exe) {
+        this.locationId = locationId;
+        this.description = description;
+        this.exits=new HashMap<>(exe);
+    }
+
+    public int getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(int locationId) {
+        this.locationId = locationId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString(){
+        return ""+this.locationId +"--->"+this.description + "--->"+this.exits;
+    }
+
+    public Map<String, Integer> getExits() {
+        return exits;
+    }
+
+    public void setExits(Map<String, Integer> exits) {
+        this.exits = exits;
+    }
+
+}
---
+++ src/mapCourse/Main.java
@@ -0,0 +1,71 @@
+package mapCourse;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner locScanner=new Scanner(System.in);
+
+        ArrayList<Integer> allPossibleLocations=new ArrayList<>();
+        allPossibleLocations.add(1);
+        allPossibleLocations.add(2);
+        allPossibleLocations.add(3);
+        allPossibleLocations.add(4);
+        allPossibleLocations.add(5);
+
+        Map<Integer,Location> locations= new HashMap<>();
+        Map<String,Integer> roadExits=new HashMap<>();
+        Map<String,Integer> hillExits=new HashMap<>();
+        Map<String,Integer> builddingExits=new HashMap<>();
+        Map<String,Integer> forrestExits=new HashMap<>();
+        Map<String,Integer> valleyExits=new HashMap<>();
+
+        // road exits
+        roadExits.put("n",5);
+        roadExits.put("w",2);
+        roadExits.put("e",3);
+        roadExits.put("s",4);
+        // road location
+        Location roadLocation=new Location(1,"Road ",roadExits);
+
+        //hill exits
+        hillExits.put("n",5);
+        //hill location
+        Location hillLocation=new Location(2,"Hill ",hillExits);
+        // buildding exits
+        builddingExits.put("w",1);
+        //building location
+        Location builldingLocation =new Location(3,"Buildding",builddingExits);
+        //forest exits
+        forrestExits.put("s",1);
+        forrestExits.put("w",2);
+        //forest location
+        Location forrestLocation=new Location(5,"Forrest",forrestExits);
+        //valley exits
+        valleyExits.put("n",1);
+        valleyExits.put("w",2);
+        // valley location
+        Location valleyLocation=new Location(4,"Valley",valleyExits);
+        // location map
+        locations.put(1,roadLocation);
+        locations.put(2,hillLocation);
+        locations.put(3,builldingLocation);
+        locations.put(4,valleyLocation);
+        locations.put(5,forrestLocation);
+
+        System.out.println(locations.get(1).getDescription());
+        Integer loc=1;
+        while (allPossibleLocations.contains(loc)){
+
+            System.out.println( locations.get(loc).getDescription());
+            loc=locScanner.nextInt();
+        }
+
+
+   /*     System.out.println(""+roadLocation+"\n"+hillLocation+"\n"+builldingLocation+"\n"+
+                valleyLocation+"\n"+forrestLocation);*/
+
+
+    }
+}
